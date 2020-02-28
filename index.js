74f3fe66ca8d77c5068788735e4850b69bbcc4b0
UTSHAGHOSH12 <30379330+UTSHAGHOSH12@users.noreply.github.com>
2020-02-28 23:13:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -210,8 +210,7 @@ app.get('/searchprofile/:name', async (req,res) => {
 	}
 })
 
-mongoose.connect(config.database, 
-	              {useNewUrlParser: true, useUnifiedTopology: true },
+mongoose.connect(config.database,
 	              () => 
 	               {console.log('Connected to MongoDB')
 	             });
