30da144653edda1379a719943b7457e1f6b20eba
Utshaghosh15 <utsaghosh2014@gmail.com>
2020-03-17 20:59:36 +0530

    New UI for the website

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	database: 'mongodb://utsa:Utsaghosh15%@ds039467.mlab.com:39467/my_profile',
+	database: 'mongodb://utsa:Utsaghosh15@ds039467.mlab.com:39467/my_profile',
 	secret: 'yoursecret'
 }
