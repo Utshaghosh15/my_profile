735331768870fac60e04d765bb3597d43e78cac9
Utshaghosh15 <utsaghosh2014@gmail.com>
2020-03-17 19:03:40 +0530

    New UI for the website

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const ProfileInfo = require('./SchemaProfile');
 const bodyparse = require('body-parser');
 const app = express();
 const config = require('./config/database')
-const passport = require('passport');
-const bcrypt = require('bcrypt');
 const port = process.env.PORT || 8080;
-const jwt = require('jsonwebtoken');
+
 
 app.use(bodyparse.json());
 app.use(cors());
