0e2603ab0f1d06d8b5926418a1578e11f5ae97b4
Utshaghosh15 <utsaghosh2014@gmail.com>
2020-03-17 21:48:58 +0530

    New UI for the website

--- config/database.js
+++ config/database.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-	database: 'mongodb://<utsha>:<Utsaghosh15%>@ds039467.mlab.com:39467/my_profile',
-=======
-	database: 'mongodb://utsa:utsaghosh15@ds039467.mlab.com:39467/my_profile',
->>>>>>> 77bde83676afd5433936dd7d976cb32655c83e12
+	database: 'mongodb://utsa:Utsaghosh15@ds039467.mlab.com:39467/my_profile',
 	secret: 'yoursecret'
 }
